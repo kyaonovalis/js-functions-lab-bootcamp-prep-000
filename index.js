3f8014e10d5888c1f01fad2f5c1c63e5414780f4
Novalis YAO <kyaonovalis@gmail.com>
2018-01-17 13:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+var name = stringA;
+function happyHolidaysTo(stringA){
+  if(stringA === $name)
+  return `Happy holidays, ${name}`
+}
+
+var holiday = stringB;
+function happyHolidayTo(stringA,stringB){
+  if( stringA === $name && stringB === $holiday) 
+  return `Happy ${holiday}, ${name}!`
+}
+
+
+function holidayCountdown(stringC, stringD) {
+    if (stringC === $days && stringD === $holidays) {
+      return `It's ${days} days until ${holiday}!`
+    } 
+}
